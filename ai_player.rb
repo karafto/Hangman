c0b782d856d28785ac9a560e6f1b0bef8b08971d
Kevin Raftogianis <kevinarafto@gmail.com>
2018-11-26 15:42:02 -0800

    Rename array to 'potential_matches'

--- ai_player.rb
+++ ai_player.rb
@@ -1,19 +1,19 @@
 class AiPlayer
-  attr_reader :guess, :words_with_same_length
+  attr_reader :guess, :potential_matches
   
   def initialize(new_word_length, word_list)
-    @words_with_same_length = word_list.select do |word|
+    @potential_matches = word_list.select do |word|
       word.length == new_word_length
     end
-    @words_with_same_length.each { |word| word.upcase! }
+    @potential_matches.each { |word| word.upcase! }
     alphabet = ('A'..'Z').to_a
     @letter_counts = {}
     alphabet.each { |letter| @letter_counts[letter] = 0 }
   end
 
-  def find_most_included_letter(words_with_same_length)
+  def find_most_included_letter(potential_matches)
     @letter_counts.each do |letter, count|
-      words_with_same_length.each do |word|
+      potential_matches.each do |word|
         @letter_counts[letter] += 1 if word.include?(letter)
       end
     end
@@ -25,15 +25,15 @@ class AiPlayer
     unless guesses_left == 9 && displayed_letters.all? { |x| x == '_' }
       if displayed_letters.include?(@guess)
         correct_indices.each do |i|
-          @words_with_same_length.delete_if { |word| word[i] != @guess }
+          @potential_matches.delete_if { |word| word[i] != @guess }
         end
       else
-        @words_with_same_length.delete_if { |word| word.include?(@guess) }
+        @potential_matches.delete_if { |word| word.include?(@guess) }
       end
 
       @letter_counts.delete(@guess)
       @letter_counts.each { |letter, count| @letter_counts[letter] = 0 }
-      find_most_included_letter(@words_with_same_length)
+      find_most_included_letter(@potential_matches)
     end
 
     loop do
